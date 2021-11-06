869449dfd1603e1fea338ba2fc76325a9f517d86
Varatharuban B <developer-at-398378596368>
2021-11-06 12:35:01 +0800

    Initial commit by ruban

---
+++ src/test/java/com/penta/PentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
